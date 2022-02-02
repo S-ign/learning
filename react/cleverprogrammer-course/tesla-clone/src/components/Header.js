a9d3d5d6c358b8c27c27669a636e870e61fbae4f
Avery Carty <avery.carty7@gmail.com>
2022-02-02 14:06:17 -0500

    update: Added burger menu and removed counter feature from src/components/

--- react/cleverprogrammer-course/tesla-clone/src/components/Header.js
+++ react/cleverprogrammer-course/tesla-clone/src/components/Header.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from "styled-components";
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 
 function Header () {
+  const [burgerStatus, setBurgerStatus] = useState(false);
 
   return (
     <Container>
@@ -17,8 +19,32 @@ function Header () {
       <RightMenu>
         <a href="">Shop</a>
         <a href="">Tesla Account</a>
-        <CustomMenu />
+        <CustomMenu 
+          onClick={()=>setBurgerStatus(true)}
+        />
       </RightMenu>
+      <BurgerNav show={burgerStatus}>
+        <CloseWrapper>
+          <CustomClose
+            onClick={()=>setBurgerStatus(false)}
+          />
+        </CloseWrapper>
+        <li><a href="#">Existing Inventory</a></li>
+        <li><a href="#">Used Inentory</a></li>
+        <li><a href="#">Trade-In</a></li>
+        <li><a href="#">Test Drive</a></li>
+        <li><a href="#">Cybertruck</a></li>
+        <li><a href="#">Roadster</a></li>
+        <li><a href="#">Semi</a></li>
+        <li><a href="#">Charging</a></li>
+        <li><a href="#">Powerwall</a></li>
+        <li><a href="#">Commercial Energy</a></li>
+        <li><a href="#">Utilities</a></li>
+        <li><a href="#">Find Us</a></li>
+        <li><a href="#">Support</a></li>
+        <li><a href="#">Investor Relations</a></li>
+      </BurgerNav>
+
     </Container>
   )
 
@@ -36,6 +62,7 @@ const Container = styled.div`
   top: 0;
   left: 0;
   right: 0;
+  z-index: 1;
 `
 
 const Menu = styled.div`
@@ -68,4 +95,38 @@ const RightMenu = styled.div`
 
 const CustomMenu = styled(MenuIcon)`
   cursor: pointer;
+`
+
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background-color: white;
+  width: 300px;
+  z-index: 100;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  transform: ${props => props.show ? 'translateX': 'translateX(100%)' };
+  transition: transform 0.2s;
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0,0,0, .2);
+  }
+
+  a {
+    font-weight: 600;
+  }
+`
+
+const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+`
+
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
 `
--- react/cleverprogrammer-course/tesla-clone/src/components/Section.js
+++ react/cleverprogrammer-course/tesla-clone/src/components/Section.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import styled from "styled-components";
+import Fade from 'react-reveal/Fade';
 
 function Section({ title,description,leftBtnText,rightBtnText,backgroundImg}) {
   return (
 
     <Wrap bgImage={backgroundImg}>
-      <ItemText>
-        <h1>{title}</h1>
-          <p>{description}</p>
-      </ItemText>
+      <Fade bottom>
+        <ItemText>
+          <h1>{title}</h1>
+            <p>{description}</p>
+        </ItemText>
+      </Fade>
       <Buttons>
-        <ButtonGroup>
-          <LeftButton>{leftBtnText}</LeftButton>
-          { rightBtnText &&
-            <RightButton>{rightBtnText}</RightButton>
-          }
-        </ButtonGroup>
+        <Fade bottom>
+          <ButtonGroup>
+            <LeftButton>{leftBtnText}</LeftButton>
+            { rightBtnText &&
+              <RightButton>{rightBtnText}</RightButton>
+            }
+          </ButtonGroup>
+        </Fade>
         <DownArrow src="/images/down-arrow.svg" />
       </Buttons>
     </Wrap>
@@ -26,6 +31,7 @@ function Section({ title,description,leftBtnText,rightBtnText,backgroundImg}) {
 export default Section
 
 const Wrap = styled.div`
+  z-index: 10;
   width: 100vw;
   height: 100vh;
   background-size: cover;
@@ -41,6 +47,7 @@ const Wrap = styled.div`
 const ItemText = styled.div`
   padding-top: 15vh;
   text-align: center;
+  z-index: 10;
 `
 
 const ButtonGroup = styled.div`
