9583e59ac35d3ef71c0499f4a2806b622cf95238
Avery Carty <avery.carty7@gmail.com>
2022-02-04 11:32:36 -0500

    update: Basic Header Added

--- react/cleverprogrammer-course/disney-clone/src/components/Header.js
+++ react/cleverprogrammer-course/disney-clone/src/components/Header.js
@@ -4,8 +4,36 @@ import styled from 'styled-components';
 function Header () {
   return (
     <Nav>
-      <Logo />
-      <NavMenu />
+      <Logo src="/images/logo.svg"/>
+
+      <NavMenu>
+        <a>
+          <img src="/images/home-icon.svg" alt="" />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src="/images/search-icon.svg" alt="" />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src="/images/watchlist-icon.svg" alt="" />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src="/images/original-icon.svg" alt="" />
+          <span>ORIGINAL</span>
+        </a>
+        <a>
+          <img src="/images/movie-icon.svg" alt="" />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src="/images/series-icon.svg" alt="" />
+          <span>SERIES</span>
+        </a>
+
+      </NavMenu>
+      <UserImg src="https://womenwriteaboutcomics.com/wp-content/uploads/2014/09/Screen-Shot-2014-09-11-at-20.43.04-227x200.png" />
     </Nav>
   )
 }
@@ -15,6 +43,9 @@ export default Header
 const Nav = styled.nav`
   height: 70px;
   background: #090b13;
+  display: flex;
+  align-item: center;
+  padding: 0 36px;
 `
 
 const Logo = styled.img`
@@ -22,4 +53,34 @@ const Logo = styled.img`
 `
 
 const NavMenu = styled.div`
+  display: flex;
+  flex: auto;
+  margin-left: 125px;
+  align-items: center;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+
+    img {
+      height: 20px;
+    }
+
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+    }
+  }
+`
+
+const UserImg = styled.img`
+  display: flex;
+  align-iems: center;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  cursor: pointer;
+  padding: 14px 0;
 `
